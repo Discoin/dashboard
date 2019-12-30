79635685e50fb12d96b18fad1bc9e78307fbe6ff
Austin Huang <im@austinhuang.me>
2019-12-30 16:30:05 -0500

    Update dashboard.tsx

--- src/components/dashboard.tsx
+++ src/components/dashboard.tsx
@@ -6,6 +6,6 @@ import CardHeader from '@material-ui/core/CardHeader';
 export const dashboardCard: React.FC = () => (
 	<Card>
 		<CardHeader title='Discoin Dashboard' />
-		<CardContent>View all the data in the Discoin database</CardContent>
+		<CardContent>Here you can see all the currencies, bots, and transactions within the Discoin API. You can also read our documentation <a href="https://discoin.gitbook.io">here</a>!</CardContent>
 	</Card>
 );
